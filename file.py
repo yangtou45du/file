8eecd1ec3c6bd7458b2749d416c404582f2d29a2
darcy <748532686@qq.com>
2018-01-05 13:54:19 +0800

    file

---
+++ file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: cp936 -*-
+#1.文件读写过程
+helloFile=open("d:\\lianxi\\file\\hello.txt",'w+')
+helloFile.write('helloContent')
+helloFile.seek(0,0)
+content=helloFile.read()
+print(content)
+print(helloFile.tell())
+helloFile.close()--
